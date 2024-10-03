709638210afb4dd2640424cbfbfdc531fb8528fe
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-10-03 23:27:20 +0530

    Definition

--- index.js
+++ index.js
@@ -22,3 +22,15 @@ function callback(contents){
 
 
 // Promise ke function ka function jb call hoga tbhi (.then wala function call hoga)
+
+// A Promise is an object representing the eventual completion or failure of an asynchronous operation.
+
+
+/*
+    A Promise is a proxy for a value not necessarily known when the promise is created. 
+    It allows you to associate handlers with an asynchronous action's eventual success value or failure reason. 
+    This lets asynchronous methods return values like synchronous methods: instead of immediately returning the final value, 
+    the asynchronous method returns a promise to supply the value at some point in the future.
+
+
+ */
